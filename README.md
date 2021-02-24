1cd094c854ba80666d0b1f9b076a2869598e147b
Sam <samxcrowley@gmail.com>
2021-02-24 22:08:24 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Bitcoin Price Tracker
+
+Displays current and historical Bitcoin prices.
+
+[Powered by CoinDesk](https://www.coindesk.com/price/bitcoin)
---
+++ src/sketch.js
@@ -0,0 +1,14 @@
+let WIDTH = 900;
+let HEIGHT = 600;
+
+function setup() {
+    
+    createCanvas(WIDTH, HEIGHT, WEBGL);
+    
+}
+
+function draw() {
+    
+    background(17);
+    
+}
